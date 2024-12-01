d868e4de4cab5e9ff1f2b43e04ce69318bc1bef4
eCreafr <raphael.lang@gmail.com>
2024-12-01 20:28:08 +0100

    modifs gitignore

--- php/php050connect.php
+++ php/php050connect.php
@@ -1,7 +1,7 @@
 <?php
 
 try {
-    // On se connecte à MySQL
+    // On se connecte à MySQL avec PDO (on voit ça dans 2 semaines en détail)
     $mysqlClient = new PDO('mysql:host=localhost;dbname=sport_2000;charset=utf8', 'root', '');
 } catch (Exception $e) {
     // En cas d'erreur, on affiche un message et on arrête tout
