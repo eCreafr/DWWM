4d68077e750cc534fca63ddd0aa9406982330dd6
eCreafr <raphael.lang@gmail.com>
2025-09-18 18:53:12 +0200

    mise a jour db ovh

--- php/php050-CRUD-final/db-ovh.php
+++ php/php050-CRUD-final/db-ovh.php
@@ -12,8 +12,8 @@ $port = 3306;
 // projetxy_cinema_movies
 
 try {
-    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $mysqlClient = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $username, $password);
+    $mysqlClient->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo "Erreur : " . $e->getMessage();
 }
